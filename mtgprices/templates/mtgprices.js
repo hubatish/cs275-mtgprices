b53b7fb7a505ed9280f18ab01e15af1d62f9dcad
hubatish <zachariah.howell@gmail.com>
2016-02-11 16:43:43 -0500

    Added some packages and made json request to kimono

--- mtgprices/templates/mtgprices.js
+++ mtgprices/templates/mtgprices.js
@@ -18,3 +18,21 @@ if (Meteor.isClient) {
     ]
     });
 }
+else{    
+    //Make call to kimono and get all generic top8 info
+    HTTP.call('GET',"https://www.kimonolabs.com/api/9utlkdbm?apikey=oUcgS30F1zkYScmzLUrafSwZ6SjPUTZp", 
+        {},
+        function(err, response) {
+            if(err){
+                console.log(err);
+            }
+            //got back JSON, parse it
+            var json = JSON.parse(response.content);
+            var deckTypes = json.results.archetypes;
+            console.log("decktypes len:"+deckTypes.length);
+            for(var i=0;i<deckTypes.length;i++){
+                //er, do something with this
+                console.log(deckTypes[i].deck.html);
+            }
+    });
+}
