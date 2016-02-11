60562a4054f88c87b3f49eb831adb3fcf5ce127e
hubatish <zachariah.howell@gmail.com>
2016-02-11 15:20:43 -0500

    Changed to other tutorial to display some text

--- mtgprices/templates/mtgprices.js
+++ mtgprices/templates/mtgprices.js
@@ -1,23 +1,20 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+    /*
+    //Installing this require package is gonna be tough
+    var request = require("request");
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    request("https://www.kimonolabs.com/api/9utlkdbm?apikey=oUcgS30F1zkYScmzLUrafSwZ6SjPUTZp", 
+        function(err, response, body) {
+            console.log(body);
+            console.log("Respone:"+response);
+    });*/
+    
+    // This code only runs on the client
+    Template.body.helpers({
+    tasks: [
+        { text: "This is task 1" },
+        { text: "This is task 2" },
+        { text: "This is task 3" }
+    ]
+    });
 }
