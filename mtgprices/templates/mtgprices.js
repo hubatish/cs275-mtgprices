a20ed67816491f29cfef90f3eb5958681d5565aa
John Wang <Jonathansw@outlook.com>
2016-03-04 17:26:32 -0500

    fixed names

--- mtgprices/templates/mtgprices.js
+++ mtgprices/templates/mtgprices.js
@@ -2,11 +2,14 @@ Router.route('/mtgprices', function () {
   this.render('mtgprices', {});
 });
 
-Archetypes = new Mongo.Collection("archetypes")
+//This will contain {name: ' ', link: ' ', avg price: '$$', decks: [unique id for each deck]}
+Archetypes = new Mongo.Collection("archetypes") //contains the deck archetypes
+
+//contains {name: ' ', link: ' ', total price: ' ', cards:[card names]}
+Decks = new Mongo.Collection("Decks") //contains the actual decks
 
 if (Meteor.isClient) {
     Meteor.subscribe("mtgPricesSubscribe");
-
     // This code only runs on the client
     Template.mtgprices.helpers({
         archetypes: function () {
@@ -15,40 +18,49 @@ if (Meteor.isClient) {
     });
 }
 else{
+  var listOfArchetypes = Archetypes.find({}).fetch();
+  console.log(listOfArchetypes.length)
+  if(listOfArchetypes.length==0){ //TODO: Check this based on date
+    var Firebase = Npm.require("firebase"); // This is the syntax for setting up a npm package in meteor
+    var mtgArchTypes = new Firebase("https://dazzling-torch-1073.firebaseio.com/kimono/api/9utlkdbm/latest/results/archetypes/");
+    var mtgDecks = new Firebase("https://dazzling-torch-1073.firebaseio.com/kimono/api/5c0jgyls/latest/results/decks/");
 
-    var listOfArchetypes = Archetypes.find({}).fetch();
-    if(listOfArchetypes.length==0){ //TODO: Check this based on date
-    
-        //only populate db if we no data already exists
-        var Firebase = Npm.require("firebase"); // This is the syntax for setting up a npm package in meteor
-        var mtgtop2Ref = new Firebase("https://dazzling-torch-1073.firebaseio.com/kimono/api/9utlkdbm/latest/results/archetypes/");
-        mtgtop2Ref.on("value", Meteor.bindEnvironment(function(snapshot){
-        var decks = snapshot.val();
-        //console.log(decks);
-        for (var i = 0; i < decks.length; i++){
-            var deckType = decks[i].deck.html;
-            var deckLink = decks[i].deck.href;
-            Archetypes.insert({
-            name: deckType,
-            link: deckLink
-            })
-        }
-        }), function(errorObject){
-        console.log("The read failed: " + errorObject.code);
-        });
-    }
-
-    Meteor.publish("mtgPricesSubscribe", function() {
-       return Archetypes.find({});
+    mtgDecks.on("value", Meteor.bindEnvironment(function(snapshot){
+      var deckList = snapshot.val();
+      for (var i = 0; i < deckList.length; i++){
+        var deckName = deckList[i].deck.text;
+        var deckLink = deckList[i].deck.href;
+        Decks.insert({
+          name: deckName,
+          link: deckLink
+          //cards: [name of cards]
+          //avg price: $$
+        })
+      }
+    }), function(errorObject){
+      console.log("The read failed: " + errorObject.code);
     });
 
+    mtgArchTypes.on("value", Meteor.bindEnvironment(function(snapshot){
+      var arch = snapshot.val();
+      for (var i = 0; i < arch.length; i++){
+        var deckType = arch[i].deck.html;
+        var deckLink = arch[i].deck.href;
+        var linkArray = _.pluck( Decks.find( { name: deckType } ).fetch(), '_id' );
+        Archetypes.insert({
+          name: deckType,
+          link: deckLink,
+          deck: linkArray
+          //avg price
+        })
+      }
+    }), function(errorObject){
+      console.log("The read failed: " + errorObject.code);
+    });
+  }
 
-    //Need to queue for top8 decks
-      //Still some errors here, need to find out how to use Meteor.bindEnvironment
-      /*
-      Archetypes.insert({
-        name: deckType,
-        link: deckLink
-      });
-      */
+  Meteor.publish("mtgPricesSubscribe", function() {
+     return Archetypes.find({});
+  });
+
 }
