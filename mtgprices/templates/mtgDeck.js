2e7ecb75030a624c8c0651107ba63a03ec9bd34d
hubatish <zachariah.howell@gmail.com>
2016-03-10 14:33:14 -0500

    Outputted deck in mtgSDeck enpoint!

--- mtgprices/templates/mtgDeck.js
+++ mtgprices/templates/mtgDeck.js
@@ -13,24 +13,26 @@ if (Meteor.isClient) {
     // This code only runs on the client
     Template.mtgSDeck.helpers( {
         deckGetC: function() {
-            return CArchetype.find({});
+            console.log("Getting decks");
+            return CDeck.find({});
         }
     });
 }
 else {
     Meteor.publish("deckSub",function(link) {
+        link = "http://mtgtop8.com/event?e=11630&d=266244&f=MO"; //hard coded example
         //Clear old collection (problem: only gets called on server start...)
         CDeck.remove({});
-        //Archetype.insert({"hello":"more" + link});
         //console.log("looking at name: "+link);
-        var allDecks = Decks.find({name:link}).fetch();
+        var allDecks = Decks.find({link:link}).fetch();
         //console.log("uh.. returend: "+JSON.stringify(allDecks));
         //console.log("publish called, results reterune: "+allDecks.length);
-        for(var i=0;i<allDecks.length;i++){
-            //console.log("deck: "+deck.name);
-            //console.log("deck: "+JSON.stringify(allDecks[i]));
-            CDeck.insert(allDecks[i]);
+        var deck = allDecks[0];
+        for(var i=0;i<deck.cards.length;i++){
+            console.log("c:"+deck.cards[i].name);
+            CDeck.insert(deck.cards[i]);
         }
+        //console.log("deck: "+JSON.stringify(allDecks[i]));
         return CDeck.find({});
     });
 }
