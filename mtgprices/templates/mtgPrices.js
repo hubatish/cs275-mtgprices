4092021060161a92d0554d03d144c673e7dc9a68
hubatish <zachariah.howell@gmail.com>
2016-03-10 14:03:54 -0500

    Updated decks database to have card array

--- mtgprices/templates/mtgPrices.js
+++ mtgprices/templates/mtgPrices.js
@@ -15,11 +15,46 @@ if (Meteor.isClient) {
 }
 else {
     var listOfArchetypes = Archetypes.find({}).fetch();
-    if (listOfArchetypes.length == 0) { //TODO: Check this based on date
+    if (listOfArchetypes.length == 0) { //TODO: Check this based on date //true){//
 
         var Firebase = Npm.require("firebase"); // This is the syntax for setting up a npm package in meteor
         var mtgArchTypes = new Firebase("https://dazzling-torch-1073.firebaseio.com/kimono/api/9utlkdbm/latest/results/archetypes/");
         var mtgDecks = new Firebase("https://dazzling-torch-1073.firebaseio.com/kimono/api/5c0jgyls/latest/results/decks/");
+        var mtgCards = new Firebase("https://dazzling-torch-1073.firebaseio.com/kimono/api/3668e6w0/latest/results");
+
+        var populateCards = function() {
+            mtgCards.on("value", Meteor.bindEnvironment(function (snapshot) {
+                //console.log("snapshot: "+JSON.stringify(snapshot));
+                var deckList = snapshot.val();
+                //console.log("cards array: "+JSON.stringify(deckList));
+                console.log("do i have length???" + deckList.length);
+                for (var i = 0; i < 4; i++) {
+                    //console.log("cards array: "+JSON.stringify(deckList[i]));
+                    var cardNames = deckList[i].collection1;
+                    var cardArray = [];
+                    for (var j = 0; j < cardNames.length; j++) {
+                        var card = cardNames[j].card;
+                        var cardNum = Number(card.slice(0, 2).trim());
+                        var cardName = card.slice(2, card.length);
+                        //console.log("cN:" + cardName);
+                        //console.log("num:" + cardNum);
+                        var newCard = {name: cardName, number: cardNum};
+                        cardArray.push(newCard);
+                    }
+                    var url = deckList[i].url;
+                    //console.log("card array length: "+cardArray.length);
+                    Decks.update({link: url},
+                        {
+                            $set: {
+                                cards: cardArray
+                            }
+                        }
+                    );
+                }
+            }), function (errorObject) {
+                console.log("The read failed: " + errorObject.code);
+            });
+        };
 
         //Todo list:
         //Need to crawl the 3rd one
@@ -34,8 +69,9 @@ else {
                     link: deckLink
                     //cards: [name of cards]
                     //avg price: $$
-                })
+                });
             }
+            populateCards();
         }), function(errorObject){
             console.log("The read failed: " + errorObject.code);
         });
