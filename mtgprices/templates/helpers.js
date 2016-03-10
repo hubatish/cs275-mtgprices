9fed03a0dd4c0d8635980e0476c9706beb10e134
Matthew Napolillo <mnapolillo42@gmail.com>
2016-03-10 10:31:47 -0500

    Working navbar and bootstrap integration!
    IT WORKSSSSSSSSKJDBASIDUHOISAFBPIAS

--- mtgprices/templates/helpers.js
+++ mtgprices/templates/helpers.js
@@ -2,7 +2,6 @@ if (Meteor.isClient) {
   Template.navItems.helpers({
     activeIfTemplateIs: function (template) {
       var currentRoute = Router.current();
-      console.log(currentRoute);
       return currentRoute &&
         template === currentRoute.lookupTemplate() ? 'active' : '';
     }
