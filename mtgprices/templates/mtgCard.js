5fa20af19a310375c082e62fd9c66e43575c8090
hubatish <zachariah.howell@gmail.com>
2016-03-11 13:07:36 -0500

    Added exceptions for aether, split / cards, lands

--- mtgprices/templates/mtgCard.js
+++ mtgprices/templates/mtgCard.js
@@ -4,7 +4,10 @@ getCardPrice = function(cardName, callback) {
     //TODO: cache this card in a database & return it if it exists
     var matchingCards = Cards.find({name:cardName}).fetch();
     if (matchingCards.length == 0) {
-        //TODO: Parse out spaces from name, replace with %20
+        //TODO: Parse out problem characters
+        cardName = cardName.replace("/","//"); //Wear / Tear -> Wear // Tear
+        cardName = cardName.replace(decodeURIComponent('%C3%86'),"Ae"); //Æther Vial -> Aether Vial
+
         var baseURL = "http://partner.tcgplayer.com/x3/phl.asmx/p?pk=JOHNWANG&s=&p=";
 
         HTTP.call('GET', baseURL + cardName,//"Flameborn%20Viron",
@@ -12,7 +15,8 @@ getCardPrice = function(cardName, callback) {
                   function(err, response) {
                       if (err) {
                           //console.log(err);
-                          console.log("Product not found: " + cardName);
+                          console.log("Error, Product not found, inserting: " + cardName);
+                          Cards.insert({name:cardName, price:0});
                           callback(0);
                           return;
                       }
